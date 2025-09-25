f4d1dfc526cd5d8097b93e49ebf21556c6076b26
firefighter-yy <yangyulun123@126.com>
2025-09-25 13:29:55 +0800

    Add files via upload

---
+++ exchange.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+inline int min(int x,int y)
+{
+    return(x<y?x:y);
+}
+int main()
+{
+    cout<<"before exchange:";
+    int a[4];
+    for(int i=0;i<4;i++){
+        cin>>a[i];
+    }
+    cout<<endl;
+
+    int m=a[0];
+    for(int i=0;i<4;i++){
+        m=min(a[i],m);
+    }
+
+    int minn=0;
+    for(int i=0;i<4;i++){
+        if(a[i]<a[minn]){
+            minn=i;
+        }
+    }
+    
+    int temp=a[minn];
+    a[minn]=a[3];
+    a[3]=temp;
+
+    cout<<"after exchange";
+    for( int i=0;i<4;i++){
+    cout<<a[i]<<" ";
+    }
+    return 0;
+}
