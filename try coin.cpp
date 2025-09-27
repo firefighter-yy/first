0bd7691158578e550b717b634192108597c6f016
firefighter-yy <yangyulun123@126.com>
2025-09-27 15:48:46 +0800

    Add files via upload

---
+++ try coin.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"请输入小于100的偶数"<<"\n";
+    int n;
+    cin>>n;
+    cout<<endl;
+    cout<<n<<endl;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i%2==1){
+
+                cout<<(j<=i?"0":"1");
+        }
+                    else{
+
+                        cout<<(j<=i?"1":"0");
+        }
+            if(j<n)
+
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
